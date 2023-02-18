8c33ceffccf772ce56ed4e40d1ba6ac32531ad84
Jannik <j.fuhrmann@micromata.de>
2023-02-18 18:19:21 +0100

    feat: Add Netlify CMS Admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,13 +10,12 @@ import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import react from "@astrojs/react";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-pv.netlify.app",
-  integrations: [
-    tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
-    sitemap(),
-  ],
+  integrations: [tailwind(), image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), sitemap(), react()]
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,22 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   mode: "jit",
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    extend: {
+      colors: {
+        primary: colors.indigo[900],
+        secondary: colors.indigo[600],
+      },
+    },
   },
   plugins: [],
 };
